e433491671fcd90c4b3b831418e5954ec29fae96
Nikola Radovic <nnradovic@gmail.com>
2019-02-19 20:25:08 +0100

    timer

--- Game.js
+++ Game.js
@@ -6,90 +6,155 @@ export default class Friends extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      point:null,
+      point: null,
       time: null,
+      totalTime: null,
       question: null,
-      correct:null,
-      questionNumber:null
+      correct: null,
+      questionNumber: null,
+      nextQuestion: false
     };
   }
 
-  componentDidMount(){
+  componentDidMount() {
 
-     apiService.getQuestions()
-    .then(res => {
-       console.log(res);
-       
-       return res
-        
-    })
-    .then(res =>{
-      this.setState({
-        time:0,
-        point:0,
-        question:res.results[0].question,
-        correctAnswer:res.results[0].correct_answer,
-        questionNumber:0
-      })
-    })
+    this.myTimer = setInterval(() => {
+      this.setState(prevState => ({
+        time: prevState.time + 1
+      }))
+    }, 1000)
 
-  }
-  
-  answerOnQuestion = (answer) =>{
-    if(answer === this.state.correctAnswer){
-  
-
-       apiService.getQuestions()
-       .then(res => {
-          console.log(res);
-          return res  
-       })
-       .then(res =>{
-         this.setState({
-           point: this.state.point+1,
-           time:0,
-           question:res.results[0].question,
-           correctAnswer:res.results[0].correct_answer,
-           questionNumber:this.state.questionNumber +1
-         })
-       })
-    }else{
-      apiService.getQuestions()
+    apiService.getQuestions()
       .then(res => {
-         console.log(res);
-         return res  
+        console.log(res);
+
+        return res
+
       })
-      .then(res =>{
+      .then(res => {
         this.setState({
-          
-          question:res.results[0].question,
-          correctAnswer:res.results[0].correct_answer,
-          questionNumber:this.state.questionNumber +1
+          time: 0,
+          point: 0,
+          question: res.results[0].question,
+          correctAnswer: res.results[0].correct_answer,
+          questionNumber: 0,
+          nextQuestion: false,
+          totalTime: 0,
         })
+        this.startCounter()
       })
+
+
+
+
+
+    if (this.state.nextQuestion) {
+
+
+
+    }
+  }
+
+
+
+
+
+
+
+  answerOnQuestion = (answer) => {
+    if (answer === this.state.correctAnswer) {
+
+
+      apiService.getQuestions()
+        .then(res => {
+          console.log(res);
+          return res
+        })
+        .then(res => {
+
+          this.setState(prevState => ({
+            totalTime: prevState.totalTime + this.state.time
+          }))
+          return res
+        })
+        .then(res => {
+          this.setState({
+            point: this.state.point + 1,
+            time: 0,
+            question: res.results[0].question,
+            correctAnswer: res.results[0].correct_answer,
+            questionNumber: this.state.questionNumber + 1,
+            nextQuestion: true
+          })
+        })
+
+
+        
+    } else {
+
+      apiService.getQuestions()
+        .then(res => {
+          console.log(res);
+          return res
+        })
+        .then(res => {
+
+          this.setState(prevState => ({
+            totalTime: prevState.totalTime + this.state.time
+          }))
+
+          return res
+        })
+        .then(res => {
+          this.setState({
+            time: 0,
+            question: res.results[0].question,
+            correctAnswer: res.results[0].correct_answer,
+            questionNumber: this.state.questionNumber + 1,
+            nextQuestion: true
+          })
+        })
     }
   }
   render() {
 
-    
+
     return (
-      <View style={styles.container}>
-        <Text>History Quiz</Text>
-        <Text>{this.state.question}</Text>
-        <Button
+
+      <View style={styles.container} >
+
+
+        {(this.state.questionNumber === 3) ?
+          <View style={styles.container} >
+          <Button
+            title="Play Another Game"
+            onPress={() =>
+              this.props.navigation.navigate('Home')
+            }
+            /> 
+            <Text>{this.state.totalTime}</Text>
+            </View>
+            :
+            <View style={styles.container}>
+            <Text>{this.state.question}</Text>
+            <Text>History Quiz</Text>
+            <Text>{this.state.question}</Text>
+            <Button
               style={styles.button}
               title='True'
-              raised 
-              onPress={()=>{this.answerOnQuestion('True')}}
-          />
-          <Button
+              raised
+              onPress={() => { this.answerOnQuestion('True') }}
+            />
+            <Button
               style={styles.button}
               title='False'
-              raised 
-              onPress={()=>{this.answerOnQuestion('False')}}
-          />
-          <Text>{this.state.point}</Text>
-          { (this.state.questionNumber === 3) ? <Text>Kraj</Text>: <Text> </Text>}
+              raised
+              onPress={() => { this.answerOnQuestion('False') }}
+            />
+            <Text>{this.state.time}</Text>
+            <Text>{this.state.point}</Text>
+          </View>
+        }
       </View>
     );
   }
