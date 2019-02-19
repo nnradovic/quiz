34298c55761b7f81d0fd37d816fc977b14630809
Nikola Radovic <nnradovic@gmail.com>
2019-02-19 23:27:32 +0100

    styling-positions

--- Game.js
+++ Game.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 import { apiService } from './apiService/apiService';
 
 export default class Friends extends React.Component {
@@ -12,8 +12,8 @@ export default class Friends extends React.Component {
       question: null,
       correct: null,
       questionNumber: null,
-      isLoading:true
-      
+      isLoading: true
+
     };
   }
 
@@ -33,12 +33,12 @@ export default class Friends extends React.Component {
       })
       .then(res => {
         this.setState({
-          isLoading:false,
+          isLoading: false,
           time: 0,
           point: 0,
           question: res.results[0].question,
           correctAnswer: res.results[0].correct_answer,
-          questionNumber: 0,
+          questionNumber: 1,
           nextQuestion: false,
           totalTime: 0,
         })
@@ -66,7 +66,7 @@ export default class Friends extends React.Component {
 
           this.setState(prevState => ({
             totalTime: prevState.totalTime + this.state.time,
-            
+
           }))
           return res
         })
@@ -77,12 +77,13 @@ export default class Friends extends React.Component {
             question: res.results[0].question,
             correctAnswer: res.results[0].correct_answer,
             questionNumber: this.state.questionNumber + 1,
-            
+
           })
         })
 
-       
-        
+
+
+
     } else {
 
       apiService.getQuestions()
@@ -107,10 +108,11 @@ export default class Friends extends React.Component {
             nextQuestion: true
           })
         })
+
     }
   }
 
-  componentWillMount(){
+  componentWillMount() {
     clearInterval(this.myTimer)
   }
   render() {
@@ -121,37 +123,43 @@ export default class Friends extends React.Component {
 
 
       <View style={styles.container} >
-        {(this.state.isLoading)? <Image source={require('../quiz/assets/Spin-1s-200px.gif')} /> : <Text></Text>}
-         
-        {(this.state.questionNumber === 10) ?
+        {(this.state.isLoading) ? <Image source={require('../quiz/assets/Spinner-1s-200px.gif')} /> : <Text></Text>}
+
+        {(this.state.questionNumber === 11) ?
           <View style={styles.container} >
-          <Button
-            title="Play again"
-            onPress={() =>
-              this.props.navigation.navigate('Home')
-            }
-            /> 
-            <Text> Total time taken to answer : {this.state.totalTime} s</Text>
-            <Text> Total score: {this.state.point}</Text>
+
+            <TouchableOpacity
+              style={styles.again}
+              onPress={() =>
+                this.props.navigation.navigate('Home')
+              }
+              underlayColor='#fff'>
+              <Text style={styles.loginText}>Play Again</Text>
+            </TouchableOpacity>
+            <Text style={styles.questionText}> Total time taken to answer : {this.state.totalTime} s</Text>
+            <Text style={styles.questionText}> Total score: {this.state.point}</Text>
+          </View>
+          :
+          <View style={styles.container}>
+            <Text style={styles.question}>Questions #{this.state.questionNumber}</Text>
+            <Text style={styles.questionText}>{this.state.question}</Text>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+              <TouchableOpacity
+                style={styles.true}
+                onPress={() => { this.answerOnQuestion('True') }}
+                underlayColor='#fff'>
+                <Text style={styles.loginText}>True</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.false}
+                onPress={() => { this.answerOnQuestion('False') }}
+                underlayColor='#fff'>
+                <Text style={styles.loginText}>False</Text>
+              </TouchableOpacity>
             </View>
-            :
-            <View style={styles.container}>
-            <Text>History Quiz</Text>
-            <Text>{this.state.question}</Text>
-            <Button
-              style={styles.button}
-              title='True'
-              raised
-              onPress={() => { this.answerOnQuestion('True') }}
-            />
-            <Button
-              style={styles.button}
-              title='False'
-              raised
-              onPress={() => { this.answerOnQuestion('False') }}
-            />
-            <Text>Elapsed time on this question: {this.state.time} s</Text>
-           
+
+            <Text style={styles.questionText}>Elapsed time on this question: {this.state.time} s</Text>
+
           </View>
         }
       </View>
@@ -162,8 +170,58 @@ export default class Friends extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fbbc05',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  questionText: {
+    color: '#575f5f',
+    fontSize: 18,
+    marginBottom: 20,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  question: {
+    color: '#575f5f',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  true: {
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#34A853',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: 150,
+    marginRight:5
+  },
+  false: {
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#EA4335',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: 150,
+    marginRight:5
+  },
+  again: {
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: '#EA4335',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#fff',
+    width: 300
+  },
+  loginText: {
+    color: '#fff',
+    textAlign: 'center',
+    paddingLeft: 10,
+    paddingRight: 10
+  }
 });
--- Home.js
+++ Home.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Platform, TouchableOpacity } from 'react-native';
 
 export default class Friends extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello, take our Quizz!!!</Text>
-                <Button
-                    title="Start Quiz"
-                    onPress={() =>
-                        this.props.navigation.navigate('Game')
-                    }
-                />
+                <Text style={styles.startText}>Hello, take our Quizz!!!</Text>
+                 
+
+                <TouchableOpacity
+                style={styles.loginScreenButton}
+                onPress={() =>
+                    this.props.navigation.navigate('Game')
+                }
+                underlayColor='#fff'>
+                <Text style={styles.loginText}>Start Quiz</Text>
+                </TouchableOpacity>
+                
+               
+                
+           
             </View>
         );
     }
@@ -20,8 +28,32 @@ export default class Friends extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#fbbc05',
         alignItems: 'center',
         justifyContent: 'center',
     },
+    startText:{
+       color:'#575f5f',
+       fontSize:24,
+       fontWeight:'bold'
+
+    },
+    loginScreenButton:{
+        marginRight:10,
+        marginLeft:10,
+        marginTop:10,
+        paddingTop:10,
+        paddingBottom:10,
+        backgroundColor:'#EA4335',
+        borderRadius:10,
+        borderWidth: 1,
+        borderColor: '#fff',
+        width:300
+      },
+      loginText:{
+          color:'#fff',
+          textAlign:'center',
+          paddingLeft : 10,
+          paddingRight : 10
+      }
 });
