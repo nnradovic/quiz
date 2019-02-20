2cad722e75234ad150dbaef714ddbed0e3607dd5
Nikola Radovic <nikola.radovic@devtech.local>
2019-02-20 11:46:25 +0100

    code-cleaning

--- Game.js
+++ Game.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { apiService } from './apiService/apiService';
 
 export default class Friends extends React.Component {
@@ -17,6 +17,7 @@ export default class Friends extends React.Component {
     };
   }
 
+  
   componentDidMount() {
 
     this.myTimer = setInterval(() => {
@@ -26,11 +27,6 @@ export default class Friends extends React.Component {
     }, 1000)
 
     apiService.getQuestions()
-      .then(res => {
-        console.log(res);
-        return res
-
-      })
       .then(res => {
         this.setState({
           isLoading: false,
@@ -42,88 +38,46 @@ export default class Friends extends React.Component {
           nextQuestion: false,
           totalTime: 0,
         })
-        this.startCounter()
+       
       })
 
   }
 
-
-
-
-
-
-
-  answerOnQuestion = (answer) => {
+  answerOnQuestion = (answer) =>{
+  apiService.getQuestions()
+  .then(res => {
+    this.setState(prevState => ({
+      totalTime: prevState.totalTime + this.state.time,
+    }))
+    return res
+  })
+  .then(res => {
+    this.setState({
+      time: 0,
+      question: res.results[0].question,
+      correctAnswer: res.results[0].correct_answer,
+      questionNumber: this.state.questionNumber + 1,
+    })
+    return res
+   })
+   .then(res=>{
     if (answer === this.state.correctAnswer) {
-
-
-      apiService.getQuestions()
-        .then(res => {
-          console.log(res);
-          return res
-        })
-        .then(res => {
-
-          this.setState(prevState => ({
-            totalTime: prevState.totalTime + this.state.time,
-
-          }))
-          return res
-        })
-        .then(res => {
-          this.setState({
-            point: this.state.point + 1,
-            time: 0,
-            question: res.results[0].question,
-            correctAnswer: res.results[0].correct_answer,
-            questionNumber: this.state.questionNumber + 1,
-
-          })
-        })
-
-
-
-
-    } else {
-
-      apiService.getQuestions()
-        .then(res => {
-          console.log(res);
-          return res
-        })
-        .then(res => {
-
-          this.setState(prevState => ({
-            totalTime: prevState.totalTime + this.state.time
-          }))
-
-          return res
-        })
-        .then(res => {
-          this.setState({
-            time: 0,
-            question: res.results[0].question,
-            correctAnswer: res.results[0].correct_answer,
-            questionNumber: this.state.questionNumber + 1,
-            nextQuestion: true
-          })
-        })
-
-    }
-  }
-
+    this.setState({
+      point: this.state.point + 1,
+    })
+   }
+  })
  
+}
+
+
   render() {
-
-
+    const { questionNumber, isLoading, totalTime, point, question, time } = this.state
+    
     return (
-
-
-
       <View style={styles.container} >
-        {(this.state.isLoading) ? <Image source={require('../quiz/assets/Spinner-1s-200px.gif')} /> : <Text></Text>}
-
-        {(this.state.questionNumber === 11) ?
+        {(isLoading) ? <Image source={require('../quiz/assets/Spinner-1s-200px.gif')} /> : null}
+        {(questionNumber === 11) ?
           <View style={styles.container} >
 
             <TouchableOpacity
@@ -134,13 +88,17 @@ export default class Friends extends React.Component {
               underlayColor='#fff'>
               <Text style={styles.loginText}>Play Again</Text>
             </TouchableOpacity>
-            <Text style={styles.questionText}> Total time taken to answer : {this.state.totalTime} s</Text>
-            <Text style={styles.questionText}> Total score: {this.state.point}</Text>
+
+            <Text style={styles.questionText}> Total time taken to answer : {totalTime} s</Text>
+            <Text style={styles.questionText}> Total score: {point}</Text>
+
           </View>
           :
           <View style={styles.container}>
-            <Text style={styles.question}>Questions #{this.state.questionNumber}</Text>
-            <Text style={styles.questionText}>{this.state.question}</Text>
+
+            <Text style={styles.question}>Questions #{questionNumber}</Text>
+            <Text style={styles.questionText}>{question}</Text>
+
             <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
               <TouchableOpacity
                 style={styles.true}
@@ -156,7 +114,7 @@ export default class Friends extends React.Component {
               </TouchableOpacity>
             </View>
 
-            <Text style={styles.questionText}>Elapsed time on this question: {this.state.time} s</Text>
+            <Text style={styles.questionText}>Elapsed time on this question: {time} s</Text>
 
           </View>
         }
